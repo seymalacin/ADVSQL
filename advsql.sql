a4d7f0af6e9bacf05c1e22c905289a9c08822274
seymalacin <seymallacinn@gmail.com>
2023-03-07 23:43:51 +0300

    07.03.23

--- advsql.sql
+++ advsql.sql
@@ -1,153 +1,320 @@
-CREATE TABLE film
+CREATE TABLE film 
 (
 id int ,
 title VARCHAR(50),
 type VARCHAR(50),
-length int
+length int 
 );
-
+    
 INSERT INTO film VALUES (1, 'Kuzuların Sessizliği', 'Korku',130);
 INSERT INTO film VALUES (2, 'Esaretin Bedeli', 'Macera', 125);
 INSERT INTO film VALUES (3, 'Kısa Film', 'Macera',40);
 INSERT INTO film VALUES (4, 'Shrek', 'Animasyon',85);
-
-CREATE TABLE actor
+CREATE TABLE actor 
 (
 id int ,
 isim VARCHAR(50),
 soyisim VARCHAR(50)
 );
+    
 INSERT INTO actor VALUES (1, 'Christian', 'Bale');
 INSERT INTO actor VALUES (2, 'Kevin', 'Spacey');
 INSERT INTO actor VALUES (3, 'Edward', 'Norton');
-
 do $$
-
 declare
-  film_count integer :=0;
-  
+    film_count integer :=0;
 begin
-  select count (*) -- kac tane film varsa sayısını getirir
-  into film_count  -- Query den gelen neticeyi film_count isimli degıskene atar
-  from film;  -- tabloyu seciyorum
-  
-  raise notice 'The number of films is %', film_count; -- % ,sareti yer tutucu olarak kullanılıyor
-
+    select count(*) -- kaç tane film varsa sayısını getirir
+    into film_count -- Query den gelen neticeyi film_count isimli değişkene atar
+    from film; -- tabloyu seçiyorum
+    
+    raise notice 'The number of films is %', film_count; -- % işareti yer tutucu olarak kullanılıyor
 end  $$ ;
-
-------------- VERİABLES - CONTAİNS----------
-DO $$ 
-DECLARE 
-  counter  integer :=1;
-  first_name  varchar(50) := 'Jhon';
-  last_name  varchar(50) := 'Done';
-  payment  numeric(4,2) :=20.5;
-  
-begin 
-  raise notice '% % % has been paid % USD',
-               counter,
-			   first_name,
-			   last_name,
-			   payment;
-end $$;			   
-
--- Task 1 : değişkenler oluşturarak ekrana " Ahmet ve Mehmet beyler 120 tl ye bilet aldılar. "" cümlesini ekrana basınız
+--***************************************************************
+--********************* VARIABLES - CONSTANT ********************
+--***************************************************************
 do $$
 declare
-	first_person	varchar(50)	:= 'Ahmet';
-	second_person 	varchar(50) := 'Mehmet';
-	payment			numeric(3)	:= 120;
+    counter     integer :=1;
+    first_name  varchar(50) := 'John';
+    last_name   varchar(50) := 'Doe';
+    payment     numeric(4,2) := 20.5;
 begin
-	raise notice '% ve % beyler % tl ye bilet aldılar',
-				first_person,
-				second_person,
-				payment;
+    raise notice '% % % has been paid % USD',
+                counter,
+                first_name,
+                last_name,
+                payment;
 end $$;
-
----------------BEKLEME KOMUTU--------------------------------
-
-DO $$
-DECLARE
-  created_at  time := now();
-begin 
-   raise notice '%', created_at;
-   perform pg_sleep(10); --- 10 sanıye beklenıyor
-   raise notice '%', created_at;  ---cıktıda aynı deger goruncek
-end $$;   
-  
---------- TABLODAN DATA TİPİNİ KOPYALAMA---------------
-   
-   /*
-		-> variable_name  table_name.column_name%type;
-		->( Tablodaki datanın aynı data türünde variable oluşturmaya yarıyor)
-	*/
+-- Task 1 : değişkenler oluşturarak ekrana " Ahmet ve Mehmet beyler 120 tl ye bilet aldılar. " 
+--              cümlesini ekrana basınız
 do $$
 declare
-	film_title film.title%type;  -- varchar;
+    first_person    varchar(50) := 'Ahmet';
+    second_person   varchar(50) := 'Mehmet';
+    payment         numeric(3)  := 120;
 begin
-	-- 1 id li filmin ismini getirelim
-	select title
-	from film
-	into film_title  -- film_title = 'Kuzuların Sessizliği'
-	where id=1;
-	
-	raise notice 'Film title id 1 : %' , film_title;
+    raise notice '% ve % beyler % tl ye bilet aldılar',
+                first_person,
+                second_person,
+                payment;
+end $$;
+--********************* BEKLETME KOMUDU **************************
+do $$
+declare
+    created_at  time := now();
+begin
+    raise notice '%', created_at;
+    perform pg_sleep(10); -- 10 saniye bekleniyor
+    raise notice '%', created_at; -- çıktıda aynı değer görünecek
+end $$;
+                
+--********************* TABLODAN DATA TİPİNİ KOPLAMA *******************
+    /*
+        -> variable_name  table_name.column_name%type;  
+        ->( Tablodaki datanın aynı data türünde variable oluşturmaya yarıyor)
+    */
+    
+do $$
+declare
+    film_title film.title%type;  -- varchar;      
+begin
+    -- 1 id li filmin ismini getirelim
+    select title
+    from film
+    into film_title  -- film_title = 'Kuzuların Sessizliği'
+    where id=1;
+    
+    raise notice 'Film title id 1 : %' , film_title;
 end $$ ;
-
 --********************* İÇ İÇE BLOK YAPILARI *******************
-
 do $$
 <<outher_block>>
 declare
-	counter	integer	:=0;
+    counter integer :=0;
 begin
-	counter := counter + 1;
-	raise notice 'The current value of counter is %', counter;
-	
-	declare
-		counter integer :=0;
-	
-	begin
-		counter := counter +10 ;
-		raise notice 'Counter in the subBlock is %', counter;
-		raise notice 'Counter in the OutherBlock is %', outher_block.counter;
-	
-	end;
-	
-	raise notice 'Counter in the outherBlock is %', counter;
-	
+    counter := counter + 1;
+    raise notice 'The current value of counter is %', counter;
+    
+    declare
+        counter integer :=0;
+    
+    begin
+        counter := counter +10 ;
+        raise notice 'Counter in the subBlock is %', counter;
+        raise notice 'Counter in the OutherBlock is %', outher_block.counter;
+    
+    end;
+    
+    raise notice 'Counter in the outherBlock is %', counter;
+    
 end outher_block $$ ;
-	
---------------- ROW TYPE------------------------------------
+-- *********************** Row Type **************************
 do $$
 declare
-	selected_actor	actor%rowtype;
+    selected_actor  actor%rowtype;
 begin
-	select *  -- id, isim, soyisim
-	from actor
-	into selected_actor -- id,isim,soyisim
-	where id=1;
-	
-	raise notice 'The actor name is % %',
-					selected_actor.isim,
-					selected_actor.soyisim;
+    select *  -- id, isim, soyisim
+    from actor
+    into selected_actor -- id,isim,soyisim
+    where id=2;
+    
+    raise notice 'The actor name is % %',
+                    selected_actor.isim,
+                    selected_actor.soyisim;
 end $$ ;
-  
-----------------------RECORD TYPE-----------
+-- *********************** Record Type *********************
+    /*
+        -> Row Type gibi çalışır ama record un tamamı değilde belli başlıkları almak 
+        istersek kullanılabilir
+		-sütunun tüm  degerlerini istiyorsak row type,  sadece belli degerleri istiyorsak, tamamı değilse record type
+    */
+    
+do $$
+declare
+    rec record; -- record data türünde rec isminde değişken oluşturuldu
+begin
+    select id,title,type
+    into rec
+    from film
+    where id = 1;
+    
+    raise notice '% % %' , rec.id, rec.title, rec.type;
+end $$ ;
+    
+------------CONSTANT----------------------------
+DO $$ 
+DECLARE
+   vat constant numeric :=0.1;
+   net_price    numeric :=20.5;
+
+begin
+   raise notice 'Satış fiyatı : % ', net_price*(1+vat);
+   -- vat := 0.25; -- constant bir ifadeyi ik setleme işleminden sonra deger degistirmeye calısırsak hata alırız
+end $$;
+
+-- constant bir ifadeye RT da deger verebiir miyim ?
+
+-- //////////////////// Control Structures ///////////////////////
+
+-- ******************** If Statement ****************
+
+-- syntax : 
+/*
+
+	if condition  then
+			statement;
+	end if ;
+
+*/
+
+-- Task : 1 id li filmi bulalım eğer yoksa ekrana uyarı yazısı verelim
+
+do $$
+declare
+	istenen_film film%rowtype;
+	istenen_filmId film.id%type := 10;
+
+begin
+	select * from film
+	into istenen_film
+	where id = istenen_filmId;
+	
+	if not found then
+		raise notice 'Girdiğiniz id li film bulunamadı : %', istenen_filmId;
+	end if;
+	
+end $$;
+
+
+----------- IF-THEN-ELSE-----------------
 
 /*
-		-> Row Type gibi çalışır ama record un tamamı değilde belli başlıkları almak
-		istersek kullanılabilir
-	*/
+
+   IF condition THEN 
+           statement ;
+   else 
+       alternative statement;
+   END IF
+   
+   
+*/
+
+-- Task : 1 idli film varsa title bilgisini yazınız yoksa uyarı yazısını ekrana basınız
+ 
+do $$ 
+declare 
+    selected_film  film % rowtype;
+	input_film_id film.id % type :=1;
+	
+
+begin 
+  select * from film 
+  into selected_film
+  where id = input_film_id;
+  
+  if not found then 
+      raise notice 'Girmiş oldugunuz id li film bulunamadı : % ', input_film_id;
+  else 
+      raise notice 'Filmin isim : %', selected_film.title;
+  end if ;
+
+end $$ ;
+
+
+--------- IF-THEN-ELSE-IF---------------
+
+
+-- syntax : 
+
+/*
+
+	IF condition_1 THEN
+				statement_1;
+		ELSEIF condition_2 THEN
+				statement_2;
+	    ELSEIF condition_3 THEN
+				statement_3;
+		ELSE 
+				statement_final;
+	END IF ;
+
+
+*/
+
+/*
+Task : 1 id li film varsa ;
+			süresi 50 dakikanın altında ise Short,
+			50<length<120 ise Medium,
+			length>120 ise Long yazalım
+*/
+
+
 do $$
 declare
-	rec	record; -- record data türünde rec isminde değişken oluşturuldu
+	v_film film%rowtype;
+	len_description varchar(50);
+
 begin
-	select id,title,type
-	into rec
-	from film
+
+	select * from film
+	into v_film  --- v_film.id = 1  / v_film.title ='Kuzuların Sessizliği'
 	where id = 1;
 	
-	raise notice '% % %' , rec.id, rec.title, rec.type;
-end $$ ;
+	if not found then
+		raise notice 'Filim bulunamadı';
+	else
+		if v_film.length > 0 and v_film.length <=50 then
+				len_description='Short';
+			elseif v_film.length>50 and v_film.length<120 then
+				len_description='Medium';
+			elseif v_film.length>120 then
+				len_description='Long';
+			else
+				len_description='Tanımlanamıyor';
+	     end if;
+	 raise notice ' % filminin süresi : %', v_film.title, len_description;
+	 end if;			
+
+end $$;
+  
+----------- Case Statement--------------
+
+-- syntax :
+ /*
+ 	CASE search-expression
+	 WHEN expression_1 [, expression_2,..] THEN
+	 	statement
+	 [..]
+	 [ELSE
+	 	else-statement]
+	 END case;
+ */
+ 
+-- Task : Filmin türüne göre çocuklara uygun olup olmadığını ekrana yazalım
+
+do $$
+declare
+	uyari varchar(50);
+	tur film.type%type;
+begin
+	select type from film
+	into tur
+	where id = 4;
 	
+	if found then 
+		case tur
+			when 'Korku' then uyari='Çocuklar için uygun değildir';
+			when 'Macera' then uyari='Çocuklar için uygun';
+			when 'Animasyon' then uyari ='Çocuklar için tavsiye edilir';
+			else
+				uyari='Tanımlanamadı';
+		end case;
+        raise notice '%', uyari;
+	end if;
+end $$;
+
+
+
+
+
